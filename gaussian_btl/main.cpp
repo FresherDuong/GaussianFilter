df3e4a9e2c372ffafc03de56db5503ef0e3b48b6
FresherDuong <duongyolo@gmail.com>
2020-05-20 23:00:18 +0700

    add desc

--- gaussian_btl/main.cpp
+++ gaussian_btl/main.cpp
@@ -1,4 +1,4 @@
-#include <opencv2/core.hpp>
+﻿#include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <cmath>
@@ -7,26 +7,26 @@
 using namespace cv;
 using namespace std;
 
+// Khởi tạo giá trị tần số cắt D, độ rộng dải W măc định 
 int D = 20;
 int W = 20;
+
 const String trackBarNameBandpass = "Bo loc Gaussian Bandpass theo tan so cat va do rong dai";
 const String trackBarNameBandstop = "Bo loc Gaussian Bandstop theo tan so cat va do rong dai";
 Mat src;
 
-// Rearrange the quadrants of a Fourier image so that the origin is at the image center
+// Sắp xếp lại các góc phần tư của hình ảnh Fourier sao cho điểm gốc nằm ở trung tâm hình ảnh
 void shiftDFT(Mat& fImage)
 {
 	Mat tmp, q0, q1, q2, q3;
 
-	// first crop the image, if it has an odd number of rows or columns
-
+	// Đầu tiên là cắt hình ảnh, nếu nó có số lượng hàng hoặc cột lẻ
 	fImage = fImage(Rect(0, 0, fImage.cols & -2, fImage.rows & -2));
 
 	int cx = fImage.cols / 2;
 	int cy = fImage.rows / 2;
 
-	// rearrange the quadrants of Fourier image
-	// so that the origin is at the image center
+	// Sắp xếp lại các góc phần tư của hình ảnh Fourier sao cho điểm gốc nằm ở trung tâm hình ảnh
 
 	q0 = fImage(Rect(0, 0, cx, cy));
 	q1 = fImage(Rect(cx, 0, cx, cy));
@@ -42,6 +42,7 @@ void shiftDFT(Mat& fImage)
 	tmp.copyTo(q2);
 }
 
+// Tạo ảnh padded
 Mat create_padded_image(Mat src)
 {
 	Mat padded;
@@ -51,6 +52,8 @@ Mat create_padded_image(Mat src)
 	return padded;
 }
 
+
+// Tạo H(u,v) cho Gaussian Bandpass filter (tham số truyền vào là tần sô cắt d0 và độ rộng dải w)
 void gaussian_bandpass_filter(Mat& dftFilter, double d0, double w)
 {
 	Mat tmp = Mat(dftFilter.rows, dftFilter.cols, CV_32F);
@@ -60,6 +63,7 @@ void gaussian_bandpass_filter(Mat& dftFilter, double d0, double w)
 	{
 		for (int j = 0; j < dftFilter.cols; j++)
 		{
+			// Công thức của bộ lọc
 			double d = pow((double)(i - center.x), 2) + pow((double)(j - center.y), 2);
 			tmp.at<float>(i, j) = (float)expf((-1.0) * pow(((d - d1) / (sqrt(d) * w)), 2));
 		}
@@ -69,6 +73,7 @@ void gaussian_bandpass_filter(Mat& dftFilter, double d0, double w)
 	merge(toMerge, 2, dftFilter);
 }
 
+// Tạo H(u,v) cho Gaussian Bandstop filter (tham số truyền vào là tần sô cắt d0 và độ rộng dải w)
 void gaussian_bandstop_filter(Mat& dftFilter, double d0, double w)
 {
 	Mat tmp = Mat(dftFilter.rows, dftFilter.cols, CV_32F);
@@ -78,6 +83,7 @@ void gaussian_bandstop_filter(Mat& dftFilter, double d0, double w)
 	{
 		for (int j = 0; j < dftFilter.cols; j++)
 		{
+			// Công thức của bộ lọc
 			double d = pow((double)(i - center.x), 2) + pow((double)(j - center.y), 2);
 			tmp.at<float>(i, j) = (float)(1.0 - expf((-1.0) * pow(((d - d1) / (sqrt(d) * w)), 2)));
 		}
@@ -87,27 +93,28 @@ void gaussian_bandstop_filter(Mat& dftFilter, double d0, double w)
 	merge(toMerge, 2, dftFilter);
 }
 
+// Áp bộ lọc vào ảnh
 Mat apply_filter(Mat img, Mat filter)
 {
 	Mat padded = create_padded_image(img);
 
-	// setup the DFT images
+	// Setup ảnh DFT
 	Mat planes[] = { Mat_<float>(padded), Mat::zeros(padded.size(), CV_32F) };
 	Mat complexI;
 	merge(planes, 2, complexI);
 
-	// do the DFT
+	// Thực hiện DFT
 	dft(complexI, complexI);
 
-	// apply filter
+	// Apply filter
 	shiftDFT(complexI);
 	mulSpectrums(complexI, filter, complexI, 0);
 	shiftDFT(complexI);
 
-	// do inverse DFT on filtered image
+	// Thực hiện inverse DFT ở ảnh đã được filtered
 	idft(complexI, complexI);
 
-	// split into planes and extract plane 0 as output image
+	// Trả về hình ảnh output
 	split(complexI, planes);
 	Mat magI = planes[0];
 	normalize(magI, magI, 0, 1, NORM_MINMAX);
@@ -115,6 +122,7 @@ Mat apply_filter(Mat img, Mat filter)
 	return magI;
 }
 
+// Thực hiện bộ lọc Bandpass filter 
 void apply_bandpass_filter(int d, int w) {
 	Mat gauss_bandpass = src.clone();
 	gaussian_bandpass_filter(gauss_bandpass, d, w);
@@ -122,6 +130,7 @@ void apply_bandpass_filter(int d, int w) {
 	imshow(trackBarNameBandpass, dst_gauss_bandpass);
 }
 
+// Thực hiện bộ lọc Bandstop filter 
 void apply_bandstop_filter(int d, int w) {
 	Mat gauss_bandstop = src.clone();
 	gaussian_bandstop_filter(gauss_bandstop, d, w);
@@ -129,6 +138,7 @@ void apply_bandstop_filter(int d, int w) {
 	imshow(trackBarNameBandstop, dst_gauss_bandstop);
 }
 
+// Hàm Callback khi chỉnh Trackbar ở ảnh thực hiện Bandpass filter
 void onChangeBandpass(int sliderValue, void* userData) {
 	int dValue = getTrackbarPos("TanSoCat", trackBarNameBandpass);
 	int wValue = getTrackbarPos("DoRongDai", trackBarNameBandpass);
@@ -136,6 +146,7 @@ void onChangeBandpass(int sliderValue, void* userData) {
 	apply_bandpass_filter(dValue, wValue);
 }
 
+// Hàm Callback khi chỉnh Trackbar ở ảnh thực hiện Bandstop filter
 void onChangeBandstop(int sliderValue, void* userData) {
 	int dValue = getTrackbarPos("TanSoCat", trackBarNameBandstop);
 	int wValue = getTrackbarPos("DoRongDai", trackBarNameBandstop);
@@ -145,14 +156,17 @@ void onChangeBandstop(int sliderValue, void* userData) {
 
 int main(int argc, char** argv)
 {
-	src = imread("Lenna.png", IMREAD_GRAYSCALE);
-	imshow("Input image", src);
+	// Ảnh gốc
+	src = imread("input_for_gaussian.png", IMREAD_GRAYSCALE);
+	imshow("Anh ban dau", src);
 
+	// Thực hiện Gaussian Bandpass Filter với độ rộng dải W và tần số cắt D
 	namedWindow(trackBarNameBandpass, 1);
 	createTrackbar("TanSoCat", trackBarNameBandpass, &D, 50, onChangeBandpass);
 	createTrackbar("DoRongDai", trackBarNameBandpass, &W, 50, onChangeBandpass);
 	apply_bandpass_filter(D, W);
 
+	// Thực hiện Gaussian Bandstop Filter với độ rộng dải W và tần số cắt D
 	namedWindow(trackBarNameBandstop, 1);
 	createTrackbar("TanSoCat", trackBarNameBandstop, &D, 50, onChangeBandstop);
 	createTrackbar("DoRongDai", trackBarNameBandstop, &W, 50, onChangeBandstop);
